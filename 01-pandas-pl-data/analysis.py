a77b60baf144d93053f5959da272d9c96616d61d
Janek <janek.glazar@gmail.com>
2026-06-26 14:01:04 +0200

    feat: mean total goals per home team

--- 01-pandas-pl-data/analysis.py
+++ 01-pandas-pl-data/analysis.py
@@ -3,8 +3,14 @@ import pandas as pd
 
 pl_data = pd.read_csv("../E0.csv")
 
-ht_goals = pl_data.groupby('HomeTeam').agg(
+home_team_goals = pl_data.groupby('HomeTeam').agg(
     gole=('FTHG', 'sum')
 ).sort_values('gole', ascending=False)
 
-print(ht_goals)
+pl_data['total_goals'] = pl_data['FTHG'] + pl_data['FTAG']
+
+mean_goals = pl_data.groupby('HomeTeam').agg(
+    srednia_goli=('total_goals', 'mean')
+).sort_values('srednia_goli', ascending=False)
+
+print(mean_goals)
