f404b8fc4aaa8e425d3d1ad9a28e15ad6363831f
Janek <janek.glazar@gmail.com>
2026-06-26 14:22:39 +0200

    feat: feat: merge match data with home team stats

--- 01-pandas-pl-data/analysis.py
+++ 01-pandas-pl-data/analysis.py
@@ -4,13 +4,17 @@ import pandas as pd
 pl_data = pd.read_csv("../E0.csv")
 
 home_team_goals = pl_data.groupby('HomeTeam').agg(
-    gole=('FTHG', 'sum')
-).sort_values('gole', ascending=False)
+    home_goals_total=('FTHG', 'sum')
+).sort_values('home_goals_total', ascending=False).reset_index()
 
 pl_data['total_goals'] = pl_data['FTHG'] + pl_data['FTAG']
 
 mean_goals = pl_data.groupby('HomeTeam').agg(
-    srednia_goli=('total_goals', 'mean')
-).sort_values('srednia_goli', ascending=False)
+    home_goals_mean=('total_goals', 'mean')
+).sort_values('home_goals_mean', ascending=False).reset_index()
 
-print(mean_goals)
+podsumowanie = home_team_goals.merge(mean_goals, on='HomeTeam')
+
+pl_data_total = pl_data.merge(podsumowanie, on='HomeTeam')
+
+print(pl_data_total)
